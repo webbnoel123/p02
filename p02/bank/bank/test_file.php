1bb3ec1abfd15b418252407db787e963c8585f8d
webbnoel123 <98321561+webbnoel123@users.noreply.github.com>
2022-01-24 12:58:18 +0100

    Delete p02/bank directory

--- p02/bank/bank/test_file.php
+++
@@ -1,4 +0,0 @@
-<?php
-  include('functions/get_account_info.php');
-  echo check_legal_account_name('hej2');
- ?>
--- p02/bank/functions/encrypt_password.php
+++
@@ -1,5 +0,0 @@
-<?php
-  function encrypt_password($password, $salt_before, $salt_after) {
-    return sha1($salt_before.$password.$salt_after);
-  } #saltar före och efter lösenordet och krypterar sedan med sha1
- ?>
