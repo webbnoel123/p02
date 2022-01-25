dae01ce8181baf222e287d008ddaada9f3fce8c6
webbnoel123 <98321561+webbnoel123@users.noreply.github.com>
2022-01-25 09:12:22 +0100

    Delete p02 directory

--- p02/action/log_out.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include('redirect.php');
-  session_start();
-  session_unset();
-  session_destroy();
-  header('location: ../?mess=du har loggat ut');
-
- ?>
--- p02/func/sql_protection.php
+++
@@ -1,5 +0,0 @@
-<?php
-  function sql_protection($str) {
-    return (str_contains($str, "'") or str_contains($str, '"') or str_contains($str, " ") or str_contains($str, "="));
-  }
-  ?>
